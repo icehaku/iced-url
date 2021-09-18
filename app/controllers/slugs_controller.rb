a25adb258f0bd5093b68fb22db5388e5b0c8e1b2
Elano Garcez <elanogarcez@gmail.com>
2021-09-18 15:00:40 -0300

    fixing tab config in editor

--- app/controllers/slugs_controller.rb
+++ app/controllers/slugs_controller.rb
@@ -1,11 +1,11 @@
 class SlugsController < ApplicationController
-	before_action :set_slug, only: %i[ stats navigate ]
-	
-	def new
-		@slug = Slug.new
-	end
+  before_action :set_slug, only: %i[ stats navigate ]
 
-	def create
+  def new
+    @slug = Slug.new
+  end
+
+  def create
     @slug = Slug.new(slug_params)
 
     respond_to do |format|
@@ -17,22 +17,22 @@ class SlugsController < ApplicationController
         format.json { render json: @slug.errors, status: :unprocessable_entity }
       end
     end
-	end
+  end
 
-	def stats
-		raise "stats"
-	end	
+  def stats
+    raise "stats"
+  end
 
-	def navigate
-		raise "navigate"
-	end
+  def navigate
+    raise "navigate"
+  end
 
   private
     def set_slug
       @slug = Slug.find_by_short_url(params[:short_url])
-    end	
+    end
 
     def slug_params
       params.require(:slug).permit(:short_url, :target_url)
-    end    	
+    end
 end
